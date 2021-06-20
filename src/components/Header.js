c377906fb6bf7d058631333ee87c1ffa1d8ff93e
jjlittle20 <66135577+jjlittle20@users.noreply.github.com>
2021-06-20 22:51:01 +0100

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Component.css";
+export const Header = () => {
+  return (
+    <div>
+      <h1 className="header">News Website</h1>
+    </div>
+  );
+};
+export default Header;
